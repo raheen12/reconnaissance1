31dd451f2006871df8ffd7d12fd3b0486d146e0e
raheen12 <106184627+raheen12@users.noreply.github.com>
2022-05-27 05:11:55 -0700

    Update reconnaissance1.sh

--- reconnaissance1.sh
+++ reconnaissance1.sh
@@ -5,9 +5,7 @@ echo "-------IP RESOLUTION-------------"
 nslookup $a 
 echo "---------WHOIS---------------"
 whois $a -H
-echo "---------------DIG-----------"
-dig $a 
 echo "-------THEHAARVESTER-----------"
-theHarvester -d $a -l 500 -b all -h recon.html 
+theHarvester -d $a -l 500 -b all -f recon.html 
 echo "-------------SUBLIST3R----------"
 sublist3r -d $a -o recon.txt
