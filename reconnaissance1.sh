dbdb24304592990a33619af6006f3e4151384412
raheen12 <106184627+raheen12@users.noreply.github.com>
2022-05-27 03:59:37 -0700

    Add files via upload

---
+++ reconnaissance1.sh
@@ -0,0 +1,15 @@
+
+echo "HELLO RAHEEN NAEEM HERE AND THIS IS MY PROJECT"
+echo "enter any domain name"
+read a
+echo "-------IP RESOLUTION-------------"
+nslookup $a 
+echo "---------WHOIS---------------"
+whois $a -H
+echo "---------------DIG-----------"
+dig $a 
+echo "-------THEHAARVESTER-----------"
+theHarvester -d $a -l 500 -b all -h recon.html 
+echo "-------------SUBLIST3R----------"
+sublist3r -d $a -o recon.txt
+
